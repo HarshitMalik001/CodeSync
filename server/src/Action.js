c6125fcec37454298dc1e943dfa0ee5c66669932
Shoib khan <shoibk897@gmail.com>
2024-12-31 05:49:07 +0530

    [MERN] - Update:Actions

--- server/src/Action.js
+++ server/src/Action.js
@@ -1,14 +1,13 @@
 const ACTIONS = {
-    JOIN: 'join',
-    JOINED: 'joined',
-    DISCONNECTED: 'disconnected',
-    CODE_CHANGE: 'code-change',
-    SYNC_CODE: 'sync-code',
-    LEAVE: 'leave',
-    CHAT_MESSAGE: 'chat-message',
-    MIC_TOGGLE: 'mic-toggle', 
-    CAMERA_TOGGLE: 'camera-toggle', 
-    MEDIA_PERMISSIONS: 'media-permission',
+    JOIN: 'join',                 // When a user joins a room
+    JOINED: 'joined',             // When a user is successfully connected to a room
+    DISCONNECTED: 'disconnected', // When a user disconnects
+    CODE_CHANGE: 'code-change',   // Collaborative code editor change
+    SYNC_CODE: 'sync-code',       // Synchronize code with new participants
+    LEAVE: 'leave',               // When a user leaves the room
+    CHAT_MESSAGE: 'chat-message', // For sending/receiving chat messages
+    NEW_PARTICIPANT: 'new-participant', // Notify when a new participant joins
+
 };
 
 export default ACTIONS;
